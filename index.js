2ac012c74a7dfd28b09e59c238e27e8f8fb192c4
Brandon Tucker <Brandon064@outlook.com>
2023-10-26 22:38:39 -0700

    calculator

---
+++ index.js
@@ -0,0 +1,49 @@
+
+function add() {
+return a + b;
+}
+function subtract(){
+return a - b;
+}
+function multiply(){
+return a * b
+}
+function divide(){
+    return a / b
+}
+
+function add(a , b){
+    return(a + b)
+}
+function subtract(a, b){
+    return(a - b)
+}
+function multiply(a, b){
+    return(a*b)
+}
+function divide(a, b){
+    return(a/b)
+}
+function increment(n){
+    n += 1
+    return(n)
+    
+}
+function decrement(n){
+    n -= 1
+    return(n)
+
+}
+function makeInt(n,a){
+    return(n)
+}
+   
+    makeInt('0x2328', 10);{
+
+    }
+    makeInt('sldkjflksjf',10);{
+
+    }
+    function preserveDecimal(n){
+return (n)
+    }
